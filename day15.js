f0de91103bfe49c4909a667fbc5a5ff2159c60bb
Yurii <yurchenko08@gmail.com>
2022-07-14 23:43:55 +0300

    added day15.1

--- day15.js
+++ day15.js
@@ -1,2 +1,10 @@
 // #22: Unlock new skills--Arrow function,spread operator and deconstruction
 // http://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+function shuffleIt(arr, ...rest) {
+  for (const el of rest) {
+    const a = el[0];
+    const b = el[1];
+    [arr[a], arr[b]] = [arr[b], arr[a]];
+  }
+  return arr;
+}
