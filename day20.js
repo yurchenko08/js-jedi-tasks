da3034fd9c09d12718a5aaf337c4fbee16b2fa36
yurchenko08 <yurchenko08@gmail.com>
2022-07-27 15:12:03 +0300

    added day20

---
+++ day20.js
@@ -0,0 +1,5 @@
+// #39: Regular Expression--""?"", ""*"", ""+"" and ""{}"" https://www.codewars.com/kata/573bca07dffc1aa693000139
+const regex1 = /^-?[9]+.*0{4,}$/;
+
+// #40: Regular Expression--""|"", ""[]"" and ""()"" http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+const regex = /http(s)?:\/\/[\w.]+\.(net|com)/gi;
