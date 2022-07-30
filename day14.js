7914fee59935e1656eaebac9f718970f8d27c1a0
Yurii <yurchenko08@gmail.com>
2022-07-30 21:51:34 +0300

    Update day14.js

--- day14.js
+++ day14.js
@@ -6,11 +6,15 @@ function matchedSkill(candidate, job) {
   for (const skill of candidate.skills) {
     if (skill.preference === "avoid") continue;
     for (const jobSkill of job.skills) {
-      if (
-        skill.name === jobSkill.name ||
-        skill.name === jobSkill.substitutions[0].name
-      )
-        firstCondition = skill;
+      if ("substitutions" in jobSkill) {
+        if (
+          skill.name === jobSkill.name ||
+          skill.name === jobSkill.substitutions
+        )
+          firstCondition = skill;
+      } else {
+        if (skill.name === jobSkill.name) firstCondition = skill;
+      }
     }
   }
   return firstCondition;
@@ -20,12 +24,20 @@ function matchedExperience(candidate, job) {
   for (const skill of candidate.skills) {
     if (skill.preference === "desired") skill.experience *= 1.5;
     for (jobSkill of job.skills) {
-      if (
-        skill.experience >= jobSkill.idealYears &&
-        (skill.name === jobSkill.name ||
-          skill.name === jobSkill.substitutions[0].name)
-      )
-        secondCondition = skill;
+      if ("substitutions" in jobSkill) {
+        if (
+          skill.experience >= jobSkill.idealYears &&
+          (skill.name === jobSkill.name ||
+            skill.name === jobSkill.substitutions)
+        )
+          secondCondition = skill;
+      } else {
+        if (
+          skill.experience >= jobSkill.idealYears &&
+          skill.name === jobSkill.name
+        )
+          secondCondition = skill;
+      }
     }
   }
   return secondCondition;
