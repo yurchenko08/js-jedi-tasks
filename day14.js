9e67500916d6263a9b5cc40568e2f1c8f1686a92
Yurii <yurchenko08@gmail.com>
2022-07-30 16:05:21 +0300

    Update day14.js

--- day14.js
+++ day14.js
@@ -1,1 +1,32 @@
 // Job Matching #3 https://www.codewars.com/kata/56c2a067585d9ac8280003c9/
+function match(candidate, job) {
+  return matchedExperience(candidate, job) && matchedSkill(candidate, job);
+}
+function matchedSkill(candidate, job) {
+  for (skill of candidate.skills) {
+    if (skill.name === job.skills[0].name && skill.preference === "avoid") {
+      return false;
+    }
+  }
+
+  for (skill of candidate.skills) {
+    if (
+      skill.name === job.skills[0].name ||
+      skill.name === job.skills[0].substitutions[0].name
+    )
+      return true;
+  }
+  return false;
+}
+function matchedExperience(candidate, job) {
+  for (skill of candidate.skills) {
+    if (skill.preference === "desired") {
+      skill.experience *= 1.5;
+    }
+    if (job.skills[0].idealYears <= skill.experience) {
+      return true;
+    }
+    continue;
+  }
+  return false;
+}
