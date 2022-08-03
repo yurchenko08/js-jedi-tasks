b5ed432bf58c67ce5116c649b951b494071ee752
yurchenko08 <yurchenko08@gmail.com>
2022-08-03 21:05:13 +0300

    added day 21

--- day21.js
+++ day21.js
@@ -1,4 +1,8 @@
 // #41: Regular Expression--"""" http://www.codewars.com/kata/573e6831e3201f6a9b000971
-const regex = /\b(\S)(\w?)(\S?)\S?\3?\2\1\b/g;
-console.log("1231 12341 123451 1234561".match(regex));
-//solved 105 tests / don't solve 4 tests
+const regex = /\b(\S)(\S?)(\S?)\S?\3\2\1\b/g;
+
+// #42: Regular Expression--(?:), (?=) and (?!) http://www.codewars.com/kata/573fb9223f9793e485000453
+const regex = /(\d)(?=(\d{3})+$)/g;
+function addCommas(money, reg) {
+  return money.replace(reg, (x) => x + ",");
+}
